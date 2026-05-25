583f251a8e84708806ea353c43f8b15272d6b370
qqer <wonters1223@gmail.com>
2026-05-25 19:29:43 +0300

    refactor: extract BaseNewsPage and implement Preview/Edit page objects

--- pages/newsPreview.page.ts
+++ pages/newsPreview.page.ts
@@ -1,44 +1,18 @@
 import { Locator, Page } from '@playwright/test';
-import { BasePage } from './base.page';
+import { BaseNewsPage } from './baseNews.page';
 
 
-
-export class PreviewNewsPage extends BasePage {
+export class NewsPreviewPage extends BaseNewsPage {
     readonly backToEditingLink: Locator;
     readonly publishButton: Locator;
-
-    readonly tagsContainer: Locator;
-    readonly tagItems: Locator;
-
-    readonly newsContentContainer: Locator;
-
-    readonly newsTitle: Locator;
-    readonly newsDate: Locator;
-    readonly newsAuthor: Locator;
-
-    readonly newsMainImage: Locator;
-
-    readonly contentText: Locator;
+    
 
     constructor(page: Page) {
         super(page);
 
         this.backToEditingLink = page.getByRole('link', { name: 'Back to editing' });
+
         this.publishButton = page.getByRole('button', { name: 'Publish' });
-
-        this.tagsContainer = page.locator('.tags');
-        this.tagItems = this.tagsContainer.locator('span, button, a');
-
-        this.newsContentContainer = page.locator('.news-content');
-
-        this.newsTitle = page.locator('.news-title');
-        this.newsDate = page.locator('.news-info-date');
-        this.newsAuthor = page.locator('.news-info-author');
-
-        this.newsMainImage = page.locator('img.news-image');
-
-        this.contentText = page.locator('.news-text-content.word-wrap');
-
     }
 
     async goBackToEditing() {
@@ -49,9 +23,5 @@ export class PreviewNewsPage extends BasePage {
     async publishNews() {
         await this.publishButton.click();
         await this.page.waitForURL('**/news');
-    }
-
-    async getTags(): Promise<string[]> {
-        return await this.tagItems.allInnerTexts();
     }
 }
